5e3576230c3e26fd03ec10dc1bc39f3e3e16db60
unknown <marcospdc1997@gmail.com>
2022-08-27 16:51:54 -0300

    css fixes and js first finished version

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const ratings = document.querySelectorAll('.btn.circle')
 const btnSubmit = document.querySelector('.btn-submit')
+let isRatingSelected = false
+let selectedScore = 0
 
 ratings.forEach(item=>{
     item.onclick = () =>{
@@ -9,14 +11,27 @@ ratings.forEach(item=>{
             item.onclick()
         } else {
             item.classList.add('selected')
+            isRatingSelected = true
+            selectedScore = item.textContent
+            console.log(selectedScore)
         }
     }
 })
 
 /* ainda tenho que pegar o valor númerico selecionado, e nao deixar clicar no botao sem selecionar uma nota */
 btnSubmit.onclick = () => {
-    const thanks = document.getElementById("thanks")
-    const rating = document.getElementById("rating")
-    thanks.style.display = "block"
-    rating.style.display = "none"
+    if(isRatingSelected){
+        printScore(selectedScore)
+        const thanks = document.getElementById("thanks")
+        const rating = document.getElementById("rating")
+        thanks.style.display = "block"
+        rating.style.display = "none"
+    } else {
+        return
+    }
+}
+
+function printScore(score){
+    const scoreRating = document.getElementById("score-selected")
+    scoreRating.innerHTML = `You selected ${score} out of 5`
 }
